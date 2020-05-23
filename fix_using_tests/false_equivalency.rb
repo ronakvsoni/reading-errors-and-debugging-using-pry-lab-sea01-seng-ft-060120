f7531e5d03ef58c669034aa47b7a95ca3845ac05
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-23 19:18:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -3,12 +3,14 @@
 def get_user_input
   gets.chomp
 end
+get_user_input
 
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
+prompt_user
 
 def selection(num)
   if num == 1
@@ -17,8 +19,10 @@ def selection(num)
      "HAM HAM HAM IN MY TUMMY"
   end
 end
+selection(1)
 
 def runner
   prompt_user
   selection(get_user_input)
 end
+puts runner
